7cbb16c8f3aff91c04472e382b4f94207d589078
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-04-25 15:18:01 +0700

    Update deploy_node.sh

--- deploy_node.sh
+++ deploy_node.sh
@@ -3,11 +3,22 @@
 echo "Deploy Node Docker, server harus kosong"
 echo "Pastikan server nginx reverse proxy dan named sudah tersedia dan dalam kondisi baru"
 echo "Pastikan IP private Node Docker dan nginx reverse proxy sudah aktif dan dapat berkomunikasi"
-echo "CTRL + C sekarang jika ini bukan server kosong atau server nginx dan named belum ada atau IP private belum bisa berkomunikasi"
-sleep 7
+echo "CTRL + C jika:"
+echo "- Ini bukan server kosong," 
+echo "- Server nginx dan named belum ada"
+echo "- IP private belum bisa terhubung"
+sleep 5
+read -p "Masukkan IP private server Node Docker: " ipprivate_node
+echo
+read -p "Masukkan IP server nginx reverse proxy: " ip_nginx
+read -p "Masukkan password root server nginx reverse proxy: " pass_nginx
+echo
+read -p "Masukkan IP server named: " ip_named
+read -p "Masukkan password root server named: " pass_named
+read -p "Masukkan ns1 yang akan named gunakan (format: ns1.domain.tld): " ns_named
 echo
 echo "Memulai proses..."
-sleep 3
+sleep 5
 
 # install library
 yum update -y
@@ -52,10 +63,83 @@ firewall-cmd --zone=public --add-service=https --permanent
 firewall-cmd --reload
 
 # buat ssh-keygen
-ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y
+#ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y
 
-# download script
-#cd /home
-#git clone git@github.com:sheratan17/docker-wp.git
+echo "Selesai. Berikutnya download script lalu koneksikan server ini dengan nginx reverse proxy dan named..."
+sleep 3
 
-echo "Selesai. Berikutnya download script lalu koneksikan server ini dengan nginx reverse proxy dan named"
+# deploy nginx
+echo "Memulai deploy nginx..."
+echo "Download script..."
+ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+cd /home
+git clone git@github.com:sheratan17/docker-wp.git
+mv /home/docker-wp/template /home/template
+cp /home/docker-wp/upload/*.sh /home/
+
+# Masukkan IP private server
+
+sed -i "s/_ipprivate_node/$ipprivate_node/g" /home/template/docker-compose.yml
+
+# Membuat nginx reverse proxy dan named
+echo
+echo "Membuat nginx reverse proxy"
+
+ssh-keyscan -t rsa $ip_nginx >> /root/.ssh/known_hosts
+
+sshpass -p "$pass_nginx" ssh-copy-id root@$ip_nginx
+ssh root@$ip_nginx "yum install epel-release -y && exit"
+ssh root@$ip_nginx "yum install nginx nano certbot python3-certbot-nginx -y && exit"
+
+# download script dan update config di nginx reverse dan named
+sed -i "s/_ipprivate_node/$ipprivate_node/g" /home/docker-wp/template-mandiri.conf.inc
+sed -i "s/_ipprivate_node/$ipprivate_node/g" /home/docker-wp/template.conf.inc
+
+scp /home/docker-wp/template-mandiri.conf.inc root@$ip_nginx:/etc/nginx/conf.d || exit 1
+scp /home/docker-wp/template.conf.inc root@$ip_nginx:/etc/nginx/conf.d || exit 1
+
+# ubah bash script agar menggunakan IP nginx
+sed -i "s/_servernginx/$ip_nginx/g" /home/2setup-php.sh
+sed -i "s/_servernginx/$ip_nginx/g" /home/2delete-php.sh
+
+ssh root@$ip_nginx "service nginx restart && exit"
+
+echo "Nginx selesai"
+echo
+echo "Memulai deploy named..."
+sleep 3
+
+today=$(date +"%Y%m%d")01
+
+# Membuat named
+echo
+echo "Membuat named"
+echo
+
+domaintanpans=$(echo $ns_named | sed 's/ns1\.//')
+
+sshpass -p "$pass_named" ssh-copy-id root@$ip_named
+
+ssh root@$ip_named "yum install bind nano bind-utils -y && exit"
+
+scp /home/docker-wp/_domain.db root@$ip_named:/etc/named || exit 1
+scp /home/docker-wp/_dns.db root@$ip_named:/etc/named || exit 1
+ssh root@$ip_named "mv /etc/named.conf /etc/named.conf.backup && exit"
+
+ssh root@$ip_named "mv /etc/named/_dns.db /etc/named/$domaintanpans.db && exit"
+scp /home/docker-wp/named.conf root@$ip_named:/etc/ || exit 1
+
+# ubah bash script agar menggunakan IP nginx dan named
+ssh "root@$ip_named" "sed -i "s/_dns/$domaintanpans/g" /etc/named/$domaintanpans.db"
+ssh "root@$ip_named" "sed -i "s/_ipnamed/$ip_named/g" /etc/named/$domaintanpans.db"
+ssh "root@$ip_named" "sed -i "s/_soa/$today/g" /etc/named/$domaintanpans.db"
+ssh "root@$ip_named" "sed -i "s/_dns/$domaintanpans/g" /etc/named.conf"
+ssh "root@$ip_named" "sed -i "s/_dns/$domaintanpans/g" /etc/named/_domain.db"
+
+sed -i "s/_servernamed/$ip_named/g" /home/2setup-php.sh
+sed -i "s/_servernamed/$ip_named/g" /home/2delete-php.sh
+
+ssh root@$ip_named "service named restart && exit"
+echo "Named selesai"
+echo
+echo "SCRIPT DEPLOY SELESAI"
