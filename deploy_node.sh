17c284ef7bbb603068618291c3348740fbd7ae1e
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-04-27 09:52:38 +0700

    Update deploy_node.sh

--- deploy_node.sh
+++ deploy_node.sh
@@ -88,7 +88,7 @@ mv /home/docker-hosting/script/changepkg-php.sh /home/
 
 # Masukkan IP private server
 
-sed -i "s/_ipprivate_node/$ipprivate_node/g" /home/wp-template/docker-compose.yml
+sed -i "s/_ipprivate_node/$ipprivate_node/g" /home/docker-hosting/wp-template/docker-compose.yml
 
 # Membuat nginx reverse proxy dan named
 echo
