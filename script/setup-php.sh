f6f119384ef16fc55bbeb064606b609773c46b58
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-05-09 15:34:12 +0700

    Update setup-php.sh

--- script/setup-php.sh
+++ script/setup-php.sh
@@ -300,7 +300,7 @@ elif [[ "$cms" == "wp" && "$ssl" == "nossl" ]]; then
 	sudo rm -f $keypath.key
 	echo "$path tidak menggunakan SSL"
 elif [[ "$cms" == "minio" && "$ssl" == "le" ]]; then	
-	sudo ssh "$user@$servernginx" "cp /etc/nginx/conf.d/minio-template.conf.inc /etc/nginx/conf.d/$path.conf && sed -i "s/_domain/$path/g" /etc/nginx/conf.d/$path.conf && sed -i "s/_randomminio/$numberminio/g"
+	sudo ssh "$user@$servernginx" "cp /etc/nginx/conf.d/minio-template.conf.inc /etc/nginx/conf.d/$path.conf && sed -i "s/_domain/$path/g" /etc/nginx/conf.d/$path.conf && sed -i "s/_randomminio/$numberminio/g" && exit"
 	sudo ssh "$user@$servernginx" "certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --must-staple --no-eff-email --staging --reinstall --email andi.triyadi@qwords.co.id -d $path -d www.$path -d file.$path -d www.file.$path -d pma.$path -d www.$path && systemctl restart nginx"
 	sudo ssh "$user@$servernginx" "sed -i 's/listen 443 ssl;/listen 443 ssl http2;/g' /etc/nginx/conf.d/$path.conf && exit"
 	sudo ssh "$user@$servernginx" "systemctl restart nginx && exit"
