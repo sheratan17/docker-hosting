174780b4eb2266a4704f97e3d9d365320813bf8f
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-05-09 15:43:34 +0700

    Update setup-php.sh

--- script/setup-php.sh
+++ script/setup-php.sh
@@ -174,9 +174,9 @@ elif [ "$cms" == "minio" ]; then
 	sudo sh -c 'echo "SITE_DOMAIN_minio='${pathtanpatitik}_minio'" >> /home/'$path'/.env'
 	sudo sh -c 'echo "MINIO_ROOT_PASSWORD='$minio_pass'" >> /home/'$path'/.env'
 	numberminio=$(shuf -i 12001-14000 -n 1)
-	#numbermini=$(shuf -i 14001-16000 -n 1)
+	numbermini=$(shuf -i 14001-16000 -n 1)
 	sudo sed -i "s/_randomminio/$numberminio/g" /home/$path/docker-compose.yml
-	#sudo sed -i "s/_randommini/$numbermini/g" /home/$path/docker-compose.yml
+	sudo sed -i "s/_randommini/$numbermini/g" /home/$path/docker-compose.yml
 fi
 
 
@@ -300,7 +300,7 @@ elif [[ "$cms" == "wp" && "$ssl" == "nossl" ]]; then
 	sudo rm -f $keypath.key
 	echo "$path tidak menggunakan SSL"
 elif [[ "$cms" == "minio" && "$ssl" == "le" ]]; then	
-	sudo ssh "$user@$servernginx" "cp /etc/nginx/conf.d/minio-template.conf.inc /etc/nginx/conf.d/$path.conf && sed -i "s/_domain/$path/g" /etc/nginx/conf.d/$path.conf && sed -i "s/_randomminio/$numberminio/g" && exit"
+	sudo ssh "$user@$servernginx" "cp /etc/nginx/conf.d/minio-template.conf.inc /etc/nginx/conf.d/$path.conf && sed -i "s/_domain/$path/g" /etc/nginx/conf.d/$path.conf && sed -i "s/_randomminio/$numberminio/g" /etc/nginx/conf.d/$path.conf && exit"
 	sudo ssh "$user@$servernginx" "certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --must-staple --no-eff-email --staging --reinstall --email andi.triyadi@qwords.co.id -d $path -d www.$path -d file.$path -d www.file.$path -d pma.$path -d www.$path && systemctl restart nginx"
 	sudo ssh "$user@$servernginx" "sed -i 's/listen 443 ssl;/listen 443 ssl http2;/g' /etc/nginx/conf.d/$path.conf && exit"
 	sudo ssh "$user@$servernginx" "systemctl restart nginx && exit"
