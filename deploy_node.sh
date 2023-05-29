fa3d7e0d3573e2d9552e61711696eab2c12e69dc
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-05-29 12:37:52 +0700

    Update deploy_node.sh

--- deploy_node.sh
+++ deploy_node.sh
@@ -148,8 +148,8 @@ ssh root@$ip_named "yum update -y && yum install bind nano lsof bind-utils polic
 
 scp /home/docker-hosting/server-template/_domain.db root@$ip_named:/etc/named || exit 1
 scp /home/docker-hosting/server-template/_dns.db root@$ip_named:/etc/named || exit 1
-scp /home/docker-hosting/server-template/named.conf root@$ip_named:/etc/ || exit 1
 ssh root@$ip_named "mv /etc/named.conf /etc/named.conf.backup && exit"
+scp /home/docker-hosting/server-template/named.conf root@$ip_named:/etc/ || exit 1
 ssh root@$ip_named "mv /etc/named/_dns.db /etc/named/$domaintanpans.db && exit"
 
 
@@ -175,8 +175,8 @@ ssh root@$ip_namedd "yum update -y && yum install bind nano lsof bind-utils poli
 
 scp /home/docker-hosting/server-template/_domain.db root@$ip_namedd:/etc/named || exit 1
 scp /home/docker-hosting/server-template/_dns.db root@$ip_namedd:/etc/named || exit 1
-scp /home/docker-hosting/server-template/_named.conf root@$ip_named:/etc/named || exit 1
 ssh root@$ip_namedd "mv /etc/named.conf /etc/named.conf.backup && exit"
+scp /home/docker-hosting/server-template/_named.conf root@$ip_named:/etc/named || exit 1
 ssh root@$ip_namedd "mv /etc/named/_dns.db /etc/named/$domaintanpans.db && exit"
 ssh root@$ip_namedd "mv /etc/_named.conf /etc/named.conf && exit"
 
