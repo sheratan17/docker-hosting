b7da1c2d8646e2dd16020e2b82ea02e8206fcfe0
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-06-13 18:28:09 +0700

    sanity input

--- script/changepkg-php.sh
+++ script/changepkg-php.sh
@@ -46,16 +46,26 @@ fi
 
 pathtanpatitik=$(echo "${path}" | sed 's/\.//g')
 
+home_path="/home/$path"
+
+# Check if folder exists
+if [ ! -d "$home_path" ]; then
+        echo "Domain tidak ditemukan. Cek input."
+        exit 1
+else
+        echo "Docker ditemukan. Melanjukan proses..."
+fi
+
 if [ "$paket" == "p1" ]; then
-        sudo docker update --memory "1g" --cpuset-cpus "1" ${pathtanpatitik}_wp
-        sudo docker update --memory "1g" --cpuset-cpus "1" ${pathtanpatitik}_db
-        sudo docker update --memory "1g" --cpuset-cpus "1" ${pathtanpatitik}_pma
+        sudo docker update --memory "1g" --memory-swap "1g" --cpuset-cpus "1" ${pathtanpatitik}_web
+        sudo docker update --memory "1g" --memory-swap "1g" --cpuset-cpus "1" ${pathtanpatitik}_db
+        sudo docker update --memory "1g" --memory-swap "1g" --cpuset-cpus "1" ${pathtanpatitik}_pma
         sudo setquota -u $path 0 1024000 0 0 -a /home
         sudo echo "User $path sudah menggunakan $paket"
 elif [ "$paket" == "p2" ]; then
-        sudo docker update --memory "2g" --cpuset-cpus "2" ${pathtanpatitik}_wp
-        sudo docker update --memory "2g" --cpuset-cpus "2" ${pathtanpatitik}_db
-        sudo docker update --memory "2g" --cpuset-cpus "2" ${pathtanpatitik}_pma
+        sudo docker update --memory "2g" --memory-swap "2g" --cpuset-cpus "2" ${pathtanpatitik}_web
+        sudo docker update --memory "2g" --memory-swap "2g" --cpuset-cpus "2" ${pathtanpatitik}_db
+        sudo docker update --memory "2g" --memory-swap "2g" --cpuset-cpus "2" ${pathtanpatitik}_pma
         sudo setquota -u $path 0 2048000 0 0 -a /home
         sudo echo "User $path sudah menggunakan $paket"
 else
