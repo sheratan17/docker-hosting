27c82f2d5e62c4022cc7f386943266eea975e181
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-06-27 11:27:07 +0700

    Update changessl-php.sh

--- script/changessl-php.sh
+++ script/changessl-php.sh
@@ -6,10 +6,17 @@ servernginx="_servernginx"
 
 read -p "Masukkan domain yang ingin dipasang SSL Let's Encrypt: " path
 read -p "Masukkan email milik klien yang domainnya ingin dipasang SSL Let's Encrypt: " email
+read -p "Masukkan CMS yang dimiliki klien (wp/minio): " cms
 
 #sudo ssh "$user@$servernginx" "certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --must-staple --no-eff-email --force-renewal --email $email -d $path -d www.$path -d file.$path -d www.file.$path -d pma.$path -d www.$path && systemctl restart nginx"
 
+if [ "$cms" == "wp" ]; then
 sudo ssh "$user@$servernginx" "certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --must-staple --no-eff-email --staging --reinstall --email $email -d $path -d www.$path -d file.$path -d www.file.$path -d pma.$path -d www.$path && systemctl restart nginx"
+fi
+
+if [ "$cms" == "minio" ]; then
+sudo ssh "$user@$servernginx" "certbot --nginx --agree-tos --redirect --hsts --staple-ocsp --must-staple --no-eff-email --staging --reinstall --email $email -d $path -d www.$path && systemctl restart nginx"
+fi
 
 sudo ssh "$user@$servernginx" "sed -i 's/listen 443 ssl;/listen 443 ssl http2;/g' /etc/nginx/conf.d/$path.conf && exit"
 
