d01f65f2d9a76f1c9cd1530334a846d2d1391bda
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-07-03 18:34:42 +0700

    Update setup-php.sh

--- script/setup-php.sh
+++ script/setup-php.sh
@@ -122,7 +122,7 @@ servernamedd="_servernameed"
 ipprivate_node="_ipprivate_node_"
 search_path="$path"
 user="root"
-mysql_host="localhost"
+mysql_host="_mysqlhost"
 mysql_user="root"
 mysql_password="_mysqlrootpass"
 
@@ -431,7 +431,7 @@ sudo rm -f $path.crt
 sudo rm -f $path.key
 
 create_table_query="CREATE TABLE IF NOT EXISTS aktivasi (id INT AUTO_INCREMENT, domain VARCHAR(255), cms VARCHAR(255), package VARCHAR(255), cert VARCHAR(255), PRIMARY KEY (id));"
-insert_query="INSERT INTO aktivasi (domain, cms, package, cert) VALUES ('$domain', '$cms', '$pkg', '$encrypt');"
+insert_query="INSERT INTO aktivasi (domain, cms, package, cert) VALUES ('$domain', '$cms', '$paket', '$encrypt');"
 
 mysql -u "$mysql_user" -p"$mysql_password" -D data_host -h"$mysql_host" -e "$create_table_query"
 mysql -u "$mysql_user" -p"$mysql_password" -D data_host -h"$mysql_host" -e "$insert_query"
