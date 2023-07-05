9bec0fcfc321a9c76b6cb5ecd2a3f356c65e222b
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-07-05 12:53:39 +0700

    Update resource.sh

--- script/resource.sh
+++ script/resource.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-container_names=$(docker ps --format "{{.Names}}")
+domain=$(docker ps --format "{{.Names}}")
+disk_quotas=$(repquota -a | tail -n+3)
 
-for container_name in $container_names; do
-cpu_usage=$(docker stats --no-stream --format "{{.CPUPerc}}" "$container_name" | sed 's/%//')
-memory_usage=$(docker stats --no-stream --format "{{.MemUsage}}" "$container_name" | awk -F'/' '{print $1}')
+for domain in $domain; do
+cpu_usage=$(docker stats --no-stream --format "{{.CPUPerc}}" "$domain" | sed 's/%//')
+memory_usage=$(docker stats --no-stream --format "{{.MemUsage}}" "$domain" | awk -F'/' '{print $1}')
 disk_usage=1
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-input_resource_query="USE docker; INSERT INTO resource (domain, cpu_usage, memory_usage, disk_usage, timestamp) VALUES ('$container_name', '$cpu_usage', '$memory_usage', 'disk_usage', '$timestamp')"
+input_resource_query="USE docker; INSERT INTO resource (domain, cpu_usage, memory_usage, disk_usage, timestamp) VALUES ('$domain', '$cpu_usage', '$memory_usage', 'disk_usage', '$timestamp')"
 
 mysql --login-path=client -e "$input_resource_query"
 done
