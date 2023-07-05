b2603e09a76f5db10e7d34115ffd681bb926601c
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-07-05 14:04:49 +0700

    update query

--- script/resource.sh
+++ script/resource.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 domain=$(docker ps --format "{{.Names}}")
-disk_quotas=$(repquota -a | tail -n+3)
 
 for domain in $domain; do
 cpu_usage=$(docker stats --no-stream --format "{{.CPUPerc}}" "$domain" | sed 's/%//')
 memory_usage=$(docker stats --no-stream --format "{{.MemUsage}}" "$domain" | awk -F'/' '{print $1}')
-disk_usage=1
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-input_resource_query="USE docker; INSERT INTO resource (domain, cpu_usage, memory_usage, disk_usage, timestamp) VALUES ('$domain', '$cpu_usage', '$memory_usage', 'disk_usage', '$timestamp')"
+input_resource_query="USE docker; INSERT INTO resource (domain, cpu_usage, memory_usage, timestamp) VALUES ('$domain', '$cpu_usage', '$memory_usage', '$timestamp')"
 
 mysql --login-path=client -e "$input_resource_query"
 done
+
+disk_quotas=$(repquota -a | tail -n+3)
+
+# Loop through each line of disk quotas and insert into the database
+while IFS= read -r line; do
+  domain=$(echo "$line" | awk '{print $1}')
+  disk_usage=$(echo "$line" | awk '{print $3}')
+  disk_usage_mb=$((disk_usage / 1024))
+
+  # Exclude unwanted lines
+  if [[ "$domain" != "root" && ! $domain =~ ^(Block|User|-+)$ ]]; then
+    query="USE docker; INSERT INTO disk (domain, disk_usage) VALUES ('$domain', $disk_usage_mb);"
+    mysql --login-path=client -e "$query"
+  fi
+done <<< "$disk_quotas"
