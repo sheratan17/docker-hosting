a45570eedbcd885a49d6bf440af3809ac82e1639
Andi Triyadi <sheratan17@users.noreply.github.com>
2023-07-05 12:48:23 +0700

    Update resource.sh

--- script/resource.sh
+++ script/resource.sh
@@ -4,9 +4,10 @@ container_names=$(docker ps --format "{{.Names}}")
 for container_name in $container_names; do
 cpu_usage=$(docker stats --no-stream --format "{{.CPUPerc}}" "$container_name" | sed 's/%//')
 memory_usage=$(docker stats --no-stream --format "{{.MemUsage}}" "$container_name" | awk -F'/' '{print $1}')
+disk_usage=1
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-input_resource_query="USE data_host; INSERT INTO docker_usage (id, domain, cpu_usage, memory_usage, timestamp) VALUES (NULL, '$container_name', '$cpu_usage', '$memory_usage', '$timestamp')"
+input_resource_query="USE docker; INSERT INTO resource (domain, cpu_usage, memory_usage, disk_usage, timestamp) VALUES ('$container_name', '$cpu_usage', '$memory_usage', 'disk_usage', '$timestamp')"
 
 mysql --login-path=client -e "$input_resource_query"
 done
